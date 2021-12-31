552ae6b8286784ac3e9f5e013267568529786446
Maruf Islam <80529512+Maruf001-MT@users.noreply.github.com>
2021-12-31 18:32:47 +0600

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,6 @@
+// function openFunction(){
+//     document.querySelector("#open").classList.remove("display");
+//     }
+//     function closeFunction(){
+//     document.querySelector("#open").classList.add("display");
+//     }
